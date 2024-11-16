14cdd6c45505b3c8ab3ea5bbb39bd68b85fe7b78
F0sya <nikitachmyria@gmail.com>
2024-11-16 14:10:28 +0200

    First hometask is completed

--- Program.cs
+++ Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int size = 10;
+            var arr = new int[size];
+            var random = new Random();
+            Console.WriteLine("Enter the lower bound of the random number: ");
+            var lowerMaxRandomBound = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the upper bound of the random number: ");
+            var upperMaxRandomBound = Convert.ToInt32(Console.ReadLine());
+
+            if (lowerMaxRandomBound > upperMaxRandomBound)
+            {
+                (lowerMaxRandomBound, upperMaxRandomBound) = (upperMaxRandomBound, lowerMaxRandomBound);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = random.Next(lowerMaxRandomBound,upperMaxRandomBound);
+            }
+
+            int even_amount = 0,odd_amount=0,unique;
+
+            Console.WriteLine("The array is: ");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+
+            for (int i =0; i < size; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    even_amount++;
+                }
+                if (arr[i] % 2 != 0)
+                {
+                    odd_amount++;
+                }
+            }
+
+            Console.WriteLine($"\nAmount of even: {even_amount}\nAmount of odd: {odd_amount}\nAmount of unique: {arr.Distinct().Count()}");
         }
     }
 }
