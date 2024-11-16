fa2b17e393d4ac6edd179790113fec93fd0025d7
F0sya <nikitachmyria@gmail.com>
2024-11-16 13:52:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Array_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
